f584288af1f5b2213f2ab582c29942ae0074afcf
Jack Somervell <jacksomervell@gmail.com>
2015-10-14 18:25:22 +0100

    couldnt seem to get the put funciton working with the factory

--- js/placeholderFactory.js
+++ js/placeholderFactory.js
@@ -1,5 +1,9 @@
 angular.module('photosApp')
 
+      // this.addPhoto = addPhoto;
+      // this.newPhoto = {};
+      // var self = this
+
 .factory('PhotoFactory', function($http, $q) {
   var PhotoFactory = {
     getPhotos: function() {
@@ -16,6 +20,23 @@ angular.module('photosApp')
 
         return deferred.promise;
     }
+
+    // addPhoto: function() {
+    //   var deferred = $q.defer();
+
+    //   $http
+    //   .post('http://jsonplaceholder.typicode.com/photos', self.newPhoto)
+    //   .success(function(response){
+    //     self.newPhoto = {} //this resets the object
+    //     deferred.resolve(response)
+    //   })
+    //   .error(function(error) {
+    //       deferred.reject(error);
+    //     })
+    //   getPhotos()
+    //   return deferred.promise;
+    // }
+    
   }
   return PhotoFactory;
 });
